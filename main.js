7b72d2e72c3d0b3d15cdacee51991cc2b7aecb19
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-02-16 10:01:42 -0500

    Added product image

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'We\'ll see awesome socks here, in a bit. I\'d update the description once the product arrives!' 
+        description: 'We\'ll see awesome socks here, in a bit. I\'d update the description once the product arrives!',
+        image: 'vmSocks-green-onWhite.jpg'
     }
 });
