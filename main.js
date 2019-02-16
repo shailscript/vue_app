30d02ceef02a56f731b4bdd3ea84cc0d5b3f3553
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-02-16 09:50:39 -0500

    App component added

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var product = "Socks";
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'We\'ll see awesome socks here, in a bit. I\'d update the description once the product arrives!' 
+    }
+});
